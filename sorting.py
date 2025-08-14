be7ce6ff73d2d67bc425991c35f43135a027ca86
Soon <jeongyeon0528@gmail.com>
2025-08-14 11:58:05 +0900

    Add time

--- sorting.py
+++ sorting.py
@@ -1,4 +1,7 @@
+import time
+
 def select(input_list):
+    start = time.time()
     for i in range(len(input_list)):
         for j in range(len(input_list)-(i+1)):
             if input_list[i] > input_list[i+j+1]:
@@ -7,5 +10,6 @@ def select(input_list):
                 input_list[i+j+1] = temp
             else:
                 continue
-
+    end = time.time()
+    print(f"select: {end - start:.6f}")
     return input_list
