799d0cb6b525a00adef134c1c9aabafbca476b90
Soon <jeongyeon0528@gmail.com>
2025-08-14 11:34:47 +0900

    Add select sorting method

---
+++ sorting.py
@@ -0,0 +1,11 @@
+def test(input_list):
+    for i in range(len(input_list)):
+        for j in range(len(input_list)-(i+1)):
+            if input_list[i] > input_list[i+j+1]:
+                temp = input_list[i]
+                input_list[i] = input_list[i+j+1]
+                input_list[i+j+1] = temp
+            else:
+                continue
+
+    return input_list
